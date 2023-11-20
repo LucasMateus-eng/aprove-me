b666baff4d55d657f38117ab03a7a3e2cd2560c4
Lucas Neris <lucas.lima.neris@gmail.com>
2023-11-20 10:46:42 -0300

    feat: validation of inputs sent on assignor routes

--- src/assignors/dto/create-assignor.dto.ts
+++ src/assignors/dto/create-assignor.dto.ts
@@ -1,15 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateAssignorDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
   @ApiProperty({ required: true })
   document: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(140)
   @ApiProperty({ required: true })
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
   @ApiProperty({ required: true })
   phone: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(140)
   @ApiProperty({ required: true })
   name: string;
 }
