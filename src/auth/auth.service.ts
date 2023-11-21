09b9f1790b78b36a8a0d03c2bc47f4d4ba031d3f
Lucas Neris <lucas.lima.neris@gmail.com>
2023-11-21 18:35:09 -0300

    feat: added logger in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {
   Injectable,
+  Logger,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -10,22 +11,33 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  private readonly USER_NOT_FOUND = 'No user found for login: ';
+  private readonly INVALID_CREDENTIALS = 'Invalid credentials';
+
+  private readonly logger = new Logger(AuthService.name);
+
   constructor(
     private prisma: PrismaService,
     private jwtService: JwtService,
   ) {}
 
   async login(login: string, password: string): Promise<AuthEntity> {
+    this.logger.log(`The user (${login}) has completed the login process`);
+
     const user = await this.prisma.user.findUnique({ where: { login: login } });
 
     if (!user) {
-      throw new NotFoundException(`No user found for login: ${login}`);
+      this.logger.error(`${this.USER_NOT_FOUND}${login}`);
+
+      throw new NotFoundException(`${this.USER_NOT_FOUND}${login}`);
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      throw new UnauthorizedException(`Invalid credentials`);
+      this.logger.error(`${this.INVALID_CREDENTIALS}`);
+
+      throw new UnauthorizedException(`${this.INVALID_CREDENTIALS}`);
     }
 
     return {
