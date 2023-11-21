176a12a1f49c6ffc718309f5af38931ecee396e2
Lucas Neris <lucas.lima.neris@gmail.com>
2023-11-21 18:35:45 -0300

    feat: added logger in PayablesService

--- src/payables/payables.service.ts
+++ src/payables/payables.service.ts
@@ -1,21 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreatePayableDto } from './dto/create-payable.dto';
 import { UpdatePayableDto } from './dto/update-payable.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PayablesService {
+  private readonly logger = new Logger(PayablesService.name);
+
   constructor(private prisma: PrismaService) {}
 
   create(createPayableDto: CreatePayableDto) {
+    this.logger.log(`Create a payable`);
+
     return this.prisma.payable.create({ data: createPayableDto });
   }
 
   findAll() {
+    this.logger.log(`Retrieve all payables`);
+
     return this.prisma.payable.findMany();
   }
 
   findOne(id: string) {
+    this.logger.log(`Retrieve payable with id ${id}`);
+
     return this.prisma.payable.findUnique({
       where: { id },
       include: { assignor: true },
@@ -23,6 +31,8 @@ export class PayablesService {
   }
 
   update(id: string, updatePayableDto: UpdatePayableDto) {
+    this.logger.log(`Update payable with id ${id}`);
+
     return this.prisma.payable.update({
       where: { id },
       data: updatePayableDto,
@@ -30,6 +40,8 @@ export class PayablesService {
   }
 
   remove(id: string) {
+    this.logger.log(`Remove payable with id ${id}`);
+
     return this.prisma.payable.delete({ where: { id } });
   }
 }
