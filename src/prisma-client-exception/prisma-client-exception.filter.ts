fb56be119fe615557ded651b3d1eee75bb6fef2b
Lucas Neris <lucas.lima.neris@gmail.com>
2023-11-20 21:26:47 -0300

    feat: exception handler from Prisma

---
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -0,0 +1,50 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    console.error(exception.message);
+
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const message = exception.message.replace(/\n/g, '');
+
+    switch (exception.code) {
+      case 'P2002': {
+        const statusCode = HttpStatus.CONFLICT;
+        response.status(statusCode).json({
+          statusCode: statusCode,
+          message: message,
+        });
+        break;
+      }
+      case 'P2003':
+      case 'P2004':
+      case 'P2005':
+      case 'P2006': {
+        const statusCode = HttpStatus.BAD_REQUEST;
+        response.status(statusCode).json({
+          statusCode: statusCode,
+          message: message,
+        });
+        break;
+      }
+      case 'P2015':
+      case 'P2018':
+      case 'P2025': {
+        const statusCode = HttpStatus.NOT_FOUND;
+        response.status(statusCode).json({
+          statusCode: statusCode,
+          message: message,
+        });
+        break;
+      }
+      default:
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
