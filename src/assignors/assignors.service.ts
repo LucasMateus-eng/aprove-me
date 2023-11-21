8de8570ef291c9f6aa2c10441b55ad7f644e2c25
Lucas Neris <lucas.lima.neris@gmail.com>
2023-11-21 18:35:28 -0300

    feat: added logger in AssignorsService

--- src/assignors/assignors.service.ts
+++ src/assignors/assignors.service.ts
@@ -1,25 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateAssignorDto } from './dto/create-assignor.dto';
 import { UpdateAssignorDto } from './dto/update-assignor.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AssignorsService {
+  private readonly logger = new Logger(AssignorsService.name);
+
   constructor(private prisma: PrismaService) {}
 
   create(createAssignorDto: CreateAssignorDto) {
+    this.logger.log(`Create a assignor`);
+
     return this.prisma.assignor.create({ data: createAssignorDto });
   }
 
   findAll() {
+    this.logger.log(`Retrieve all assignors`);
+
     return this.prisma.assignor.findMany();
   }
 
   findOne(id: string) {
+    this.logger.log(`Retrieve assignor with id ${id}`);
+
     return this.prisma.assignor.findUnique({ where: { id } });
   }
 
   update(id: string, updateAssignorDto: UpdateAssignorDto) {
+    this.logger.log(`Update assignor with id ${id}`);
+
     return this.prisma.assignor.update({
       where: { id },
       data: updateAssignorDto,
@@ -27,6 +37,8 @@ export class AssignorsService {
   }
 
   remove(id: string) {
+    this.logger.log(`Remove assignor with id ${id}`);
+
     return this.prisma.assignor.delete({ where: { id } });
   }
 }
